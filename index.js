2cb504cb7b6790eaf5954924418148d3c74ea832
kevin <kthomes13@gmail.com>
2023-11-29 13:55:08 -0600

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki']
+
+function writeCards(names, surprise) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`)
+    }
+    return messages
+} 
+
+let counter = 10
+
+function countDown(counter) {
+    while (counter >= 0) {
+        console.log(counter--)
+}
+}
